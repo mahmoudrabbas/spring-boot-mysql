38d01bb143eb0566de94e2d6e531372907152c7c
mahmoudrabbas <mahmoudrabass11@gmail.com>
2025-04-18 03:14:56 +0200

    fix some issues

--- src/main/java/com/store/config/openApi/OpenApiConfig.java
+++ src/main/java/com/store/config/openApi/OpenApiConfig.java
@@ -4,23 +4,37 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    @Value("${app.title}")
+    private String appTitle;
+    @Value("${app.version}")
+    private String version;
+    @Value("${app.description}")
+    private String description;
+    @Value("${app.developer.name}")
+    private String appDeveloperName;
+    @Value("${app.developer.email}")
+    private String appDeveloperEmail;
+    @Value("${app.developer.github}")
+    private String appDeveloperGithub;
+
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("${app.title}")
-                        .version("${app.version}")
-                        .description("${app.description}")
+                        .title(appTitle)
+                        .version(version)
+                        .description(description)
                         .contact(new Contact()
-                                .name("${app.developer.name}")
-                                .email("${app.developer.email}")
-                                .url("${app.developer.github}"))
+                                .name(appDeveloperName)
+                                .email(appDeveloperEmail)
+                                .url(appDeveloperGithub))
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://springdoc.org"))
--- src/main/java/com/store/controller/PostController.java
+++ src/main/java/com/store/controller/PostController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/my-posts")
 public class PostController {
+
     @Autowired
     private PostService postService;
 
